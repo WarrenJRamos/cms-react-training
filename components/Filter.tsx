4d3db7196a22b149d695f3800cf5ecf374a48751
warrenjramos <wjnramos@gmail.com>
2022-12-06 14:29:59 -0700

    Finished filter logic for now

---
+++ components/Filter.tsx
@@ -0,0 +1,144 @@
+import React, { ChangeEventHandler, useContext, useEffect, useState } from "react";
+import Context from "../context/index-store";
+import useRequest from "../hooks/use-request";
+
+const characterFilterOptions = [
+    { value: "", text: "Character" },
+    { value: "1009368", text: "Iron Man" },
+    { value: "1009220", text: "Captain America" },
+    { value: "1009664", text: "Thor" },
+    { value: "1009268", text: "Deadpool" },
+    { value: "1009562", text: "Scarlet Witch" },
+    { value: "1009189", text: "Black Widow" },
+    { value: "1009707", text: "Wasp" },
+    { value: "1010763", text: "Gamora" },
+];
+
+const creatorFilterOptions = [
+    { value: "", text: "Creator" },
+    { value: "12787", text: "Kate Leth" },
+    { value: "24", text: "Brian Michael Bendis" },
+    { value: "30", text: "Stan Lee" },
+    { value: "32", text: "Steve Ditko" },
+    { value: "196", text: "Jack Kirby" },
+];
+export const Filter = () => {
+    const {
+        getMarvelComicsResourceUrl,
+        fetchData,
+        isLoading,
+        setIsLoading,
+        isSuccess,
+        setIsSuccess,
+        hasError,
+        setHasError,
+    } = useRequest();
+    const [selectedCharacter, setSelectedCharacter] = useState<string>(
+        characterFilterOptions[0].value
+    );
+    const [selectedCreator, setSelectedCreator] = useState<string>(
+        creatorFilterOptions[0].value
+    );
+    const context = useContext(Context);
+
+    const onCharacterChange: ChangeEventHandler<HTMLSelectElement> = (
+        event
+    ) => {
+        console.log(event.target.value);
+        setSelectedCharacter(event.target.value);
+    };
+
+    const onCreatorChange: ChangeEventHandler<HTMLSelectElement> = (event) => {
+        console.log(event.target.value);
+        setSelectedCreator(event.target.value);
+    };
+
+    const fetchFilteredComics = () => {
+        let path: string;
+        if (selectedCharacter && selectedCreator) {
+            console.log("Character and Creator Selected");
+            path = getMarvelComicsResourceUrl(
+                `https://gateway.marvel.com/v1/public/creators/${selectedCreator}/comics?characters=${selectedCharacter}&`
+            );
+        } else if (selectedCharacter) {
+            console.log("Character Selected");
+            path = getMarvelComicsResourceUrl(
+                `https://gateway.marvel.com/v1/public/characters/${selectedCharacter}/comics?`
+            );
+        } else if (selectedCreator) {
+            console.log("Creator Selected");
+            path = getMarvelComicsResourceUrl(
+                `https://gateway.marvel.com/v1/public/creators/${selectedCreator}/comics?`
+            );
+        }
+        console.log(`Sending a request to ${path}`);
+        fetchData({ endpoint: path })
+            .then((data) => {
+                console.log(data);
+                context.setComics(data.data.results);
+                setIsLoading(false);
+                setIsSuccess(true);
+            })
+            .catch((error) => {
+                setHasError(error);
+                setIsSuccess(false);
+                setIsLoading(false);
+            });
+    };
+
+    const fetchDefaultComics = () => {
+        fetchData({ endpoint: getMarvelComicsResourceUrl("https://gateway.marvel.com/v1/public/comics?") })
+            .then((data) => {
+                console.log(data);
+                context.setComics(data.data.results);
+                setIsLoading(false);
+                setIsSuccess(true);
+            })
+            .catch((error) => {
+                setHasError(error);
+                setIsSuccess(false);
+                setIsLoading(false);
+            });
+    }
+
+    useEffect(() => {
+        console.log(
+            `Inside useEffect - Selected Character: ${
+                selectedCharacter || "N/A"
+            }, Selected Creator: ${selectedCreator || "N/A"}`
+        );
+        if (selectedCharacter || selectedCreator) {
+            fetchFilteredComics();
+        }
+        if (!selectedCharacter && !selectedCreator) {
+            fetchDefaultComics();
+        }
+    }, [selectedCharacter, selectedCreator]);
+
+    return (
+        <div>
+            Filter by:
+            <select value={selectedCharacter} onChange={onCharacterChange}>
+                {characterFilterOptions.map((charOption) => {
+                    return (
+                        <option value={charOption.value} key={charOption.value}>
+                            {charOption.text}
+                        </option>
+                    );
+                })}
+            </select>
+            <select value={selectedCreator} onChange={onCreatorChange}>
+                {creatorFilterOptions.map((creatorOption) => {
+                    return (
+                        <option
+                            value={creatorOption.value}
+                            key={creatorOption.value}
+                        >
+                            {creatorOption.text}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+};
